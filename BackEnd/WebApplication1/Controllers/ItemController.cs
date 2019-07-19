ce388db0596ae7a5837e9bfbbde6ae06278aaa23
XiaopingLumed <xyu@lumed.ca>
2019-07-19 13:15:18 -0400

    feature/change-name-item-service-Update()&indention

--- BackEnd/WebApplication1/Controllers/ItemController.cs
+++ BackEnd/WebApplication1/Controllers/ItemController.cs
@@ -1,8 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using Guideline.Models;
@@ -12,7 +8,7 @@ namespace Guideline.Controllers
 {
   [EnableCors(origins: "*", headers: "*", methods: "*")]
   public class ItemController : ApiController
-   {
+  {
     private ItemService service = new ItemService();
 
     public IEnumerable<Item> GetAllMainSections()
@@ -27,29 +23,22 @@ namespace Guideline.Controllers
 
     public Item Post(int id, [FromBody]ItemType type)
     {
-        return service.createNewItemAndAddTo(id,type); 
+      return service.createNewItemAndAddTo(id, type);
     }
 
     public Item Post([FromBody]ItemType type)
     {
-        return service.createNewItem(type);
+      return service.createNewItem(type);
     }
 
-    public HttpResponseMessage Put( [FromBody]Item some)
+    public Item Put([FromBody]Item itemToUpdate)
     {
-       if (ModelState.IsValid)
-      {
-        return service.Put(some);
-      }
-      else
-      {
-        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-      }
+      return service.Update(itemToUpdate);
     }
 
     public void Delete(int id)
     {
       service.Delete(id);
     }
-   }
+  }
 }
