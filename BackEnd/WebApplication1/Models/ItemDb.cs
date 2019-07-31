fbdf435c06e0f1f2848890c542bbbe565c2f0ee3
XiaopingLumed <xyu@lumed.ca>
2019-07-31 13:50:30 -0400

    add "path" in Web.config/appSettings

--- BackEnd/WebApplication1/Models/ItemDb.cs
+++ BackEnd/WebApplication1/Models/ItemDb.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using Newtonsoft.Json;
 
@@ -10,12 +11,12 @@ namespace Guideline.Models
     public static void Serialize()
     {
       string jsonResult = JsonConvert.SerializeObject(ITEMS, Formatting.Indented);
-      File.WriteAllText(@"c:\Source\lumed-guidelines\editedGuideline.json", jsonResult);
+      File.WriteAllText(@ConfigurationManager.AppSettings["SavePath"], jsonResult);
     }
 
     public static void Deserialize()
     {
-        ITEMS = JsonConvert.DeserializeObject<List<Item>>(File.ReadAllText(@"c:\Source\lumed-guidelines\guideline.json"));
+        ITEMS = JsonConvert.DeserializeObject<List<Item>>(File.ReadAllText(@ConfigurationManager.AppSettings["GuidelinePath"]));
     }
     public static List<Item> ITEMS; 
     public static int LastId = 101;
